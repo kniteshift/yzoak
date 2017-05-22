f836eee22f1bef932319204c0d7c7a2863f5c3d3
alan <alanleunglee@gmail.com>
2017-05-22 14:49:11 -0700

    updated video

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -4,20 +4,40 @@ window.$.velocity = require('velocity-animate/velocity.js')
 
 import Materialze from 'materialize-css/dist/js/materialize.js'
 import { WorksWith, Features } from './append'
-import { mapIcons } from './listener'
+import { mapIcons, callPlayer } from './listener'
 
 $(function(){
 
-    let options = [
+    // Trigger scrollfire
+    let scrollOptions = [
         {selector: '.take-control', offset:200, callback: (el) => {
             console.log('test')
             $('.nav-wrapper').addClass('navbar-black')
         }}
     ]
-    Materialize.scrollFire(options)
+    Materialize.scrollFire(scrollOptions)
 
+    // Map the Icons to the DOM Progammatically
     mapIcons(WorksWith, 'works', '#works-with-icons')
     mapIcons(Features, 'feat', '#feats')
 
+    // Init ToolTip
     $('.tooltipped').tooltip({'delay': 50})
+
+    let modalOptions = {
+        dismissible: true,
+        opacity: .5,
+        inDuration: 300,
+        outDuration: 200,
+        complete: () => {
+                var $this = $(this).find('iframe'),
+                tempSrc = $this.attr('src');
+                $this.attr('src', "");
+                $this.attr('src', tempSrc);
+        }
+    }
+
+    $('.modal').modal(modalOptions)
+        
+     
 })
